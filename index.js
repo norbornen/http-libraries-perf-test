eb76238853c4439bf1e5f511b68f83003a5c21d1
Alexey Kolpakov <al.kolpak@gmail.com>
2019-07-22 19:24:18 +0300

    impr

--- index.js
+++ index.js
@@ -1,130 +1,126 @@
-const got = require('got');
-const nock = require('nock');
-
-nock('https://sindresorhus.com')
-    .get('/')
-    .reply(200, 'Hello world!');
-
-(async () => {
-    const response = await got('https://sindresorhus.com', {json: true});
-    console.log(response.body);
-    //=> 'Hello world!'
-})();
-
-/*
+const createTestServer = require('create-test-server');
+const Benchmark = require('benchmark');
 const http = require('http');
+const https = require('https');
 const axios = require('axios');
 const got = require('got');
 const superagent = require('superagent');
 const request = require('request');
 
-const nock = require('nock');
-const HOST = 'test-perf.com';
-
-axios.defaults.baseURL = `http://${HOST}`;
-
-const Benchmark = require('benchmark');
-const suite = new Benchmark.Suite;
-
-nock(`http://${HOST}`).persist()
-    // .log(console.log)
-    .get('/test').reply(200, 'ok')
-    .post('/test').reply(200, 'ok');
-
-suite.add('http.request POST request', {
-    defer: true,
-    fn: (defer) => {
-        const req = http.request({ host: HOST, path: '/test', method: 'POST' }, (res) => {
-            res.resume().on('end', () => defer.resolve());
-        });
-        req.write();
-        req.end();
+(async () => {
+    const server = await createServer();
+    try {
+        await runSuite(server.url);
+        // await runSuite(server.sslUrl);
+    } catch (err) {
+        console.error(err);
     }
-});
+    await server.close();
+})();
 
-suite.add('http.request GET request', {
-    defer: true,
-    fn: (defer) => {
-        http.request({ path: '/test', host: HOST }, (res) => {
-            res.resume().on('end', () => defer.resolve());
-        }).end();
-    }
-});
-
-suite.add('axios GET request', {
-    defer: true,
-    fn: (defer) => {
-        axios.get('/test').then(() => defer.resolve());
-    }
-});
-
-suite.add('axios POST request', {
-    defer: true,
-    fn: (defer) => {
-        axios.post('/test').then(() => defer.resolve());
-    }
-});
-
-suite.add('got GET request', {
-    defer: true,
-    fn: async (defer) => {
-        try {
-            const r = await got.get(`http://${HOST}/test`);
-            console.log(r);
-            
-        } catch (err) {
-            console.log(err);
+async function createServer() {
+    const server = await createTestServer({
+        bodyParser: {
+            type: () => false
         }
-        defer.resolve();
-        process.exit(1)
-        // got.get(`http://${HOST}/test`).then(() => defer.resolve()).catch((err) => defer.resolve(err));
-    }
-});
+    });
+    server.get('/test', 'answer');
+    server.post('/test', (req, res) => res.send('answer'));
+    return server;
+}
 
-suite.add('got POST request', {
-    defer: true,
-    fn: (defer) => {
-        // got.post(`http://${HOST}/test`).then(() => defer.resolve()).catch((err) => defer.resolve(err));
-    }
-});
+async function runSuite(HOST) {
+    return new Promise((resolve, reject) => {
+        const suite = new Benchmark.Suite('test-perf', {
+            onAbort: (e) => reject(e),
+            onError: (e) => reject(e),
+            onReset: (e) => reject(e),
+            onCycle: (ev) => console.log(String(ev.target)),
+            onComplete: function(ev) {
+                const fastest = this.filter('fastest').map('name');
+                console.log(`\nFastest is "${fastest}"\n`);
+                resolve();
+            }
+        });
 
-suite.add('superagent GET request', {
-    defer: true,
-    fn: (defer) => {
-        superagent.get(`http://${HOST}/test`).end(() => defer.resolve());
-    }
-});
+        const endpoint = `${HOST}/test`;
 
-suite.add('superagent POST request', {
-    defer: true,
-    fn: (defer) => {
-        superagent.post(`http://${HOST}/test`).send().end(() => defer.resolve());
-    }
-});
+        suite.add('http.request POST request', {
+            defer: true,
+            fn: (defer) => {
+                const req = http.request(endpoint, { method: 'POST' }, (res) => {
+                    res.resume().on('end', () => defer.resolve());
+                });
+                req.write('');
+                req.end();
+            }
+        });
 
-suite.add('Request GET request', {
-    defer: true,
-    fn: (defer) => {
-        request(`http://${HOST}/test`, () => defer.resolve());
-    }
-});
+        suite.add('http.request GET request', {
+            defer: true,
+            fn: (defer) => {
+                http.request(endpoint, (res) => {
+                    res.resume().on('end', () => defer.resolve());
+                }).end();
+            }
+        });
 
-suite.add('Request POST request', {
-    defer: true,
-    fn: (defer) => {
-        request.post({ url: `http://${HOST}/test` }, () => defer.resolve());
-    }
-});
+        suite.add('Axios POST request', {
+            defer: true,
+            fn: (defer) => {
+                axios.post(endpoint).then(() => defer.resolve()); // .catch((err) => defer.reject(err));
+            }
+        });
 
-suite.on('complete', function(defer) {
-    console.log('Fastest is ' + this.filter('fastest').map('name'));
-});
+        suite.add('Axios GET request', {
+            defer: true,
+            fn: (defer) => {
+                axios.get(endpoint).then(() => defer.resolve());
+            }
+        });
 
-suite.on('cycle', function(event) {
-    console.log(String(event.target));
-});
+        suite.add('Got POST request', {
+            defer: true,
+            fn: (defer) => {
+                got.post(endpoint).then(() => defer.resolve());
+            }
+        });
 
-suite.run({ async: true });
+        suite.add('Got GET request', {
+            defer: true,
+            fn: (defer) => {
+                got.get(endpoint).then(() => defer.resolve());
+            }
+        });
 
-*/
+        suite.add('Superagent POST request', {
+            defer: true,
+            fn: (defer) => {
+                superagent.post(endpoint).send().end(() => defer.resolve());
+            }
+        });
 
+        suite.add('Superagent GET request', {
+            defer: true,
+            fn: (defer) => {
+                superagent.get(endpoint).end(() => defer.resolve());
+            }
+        });
+
+        suite.add('Request POST request', {
+            defer: true,
+            fn: (defer) => {
+                request.post({ url: endpoint }, () => defer.resolve());
+            }
+        });
+
+        suite.add('Request GET request', {
+            defer: true,
+            fn: (defer) => {
+                request(endpoint, () => defer.resolve());
+            }
+        });
+
+        suite.run({ async: true });
+    });
+}
