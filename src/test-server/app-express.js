b940c7b446215eaf1355898cff0ae5d1229fbccb
Alexey Kolpakov <al.kolpak@gmail.com>
2021-07-05 01:17:50 +0300

    .

---
+++ src/test-server/app-express.js
@@ -0,0 +1,28 @@
+// @ts-check
+// import express from 'express';
+
+// const app = express();
+// app.use(express.urlencoded({ extended: false }));
+// app.use(express.json());
+
+
+// app.get('/test', (_, res, next) => {
+//   res.type('text/html');
+//   res.send('answer');
+//   next();
+// });
+
+// app.post('/test', (_, res, next) => {
+//   res.type('text/html');
+//   res.send('answer');
+//   next();
+// });
+
+// app.on('error', err => {
+//   console.error('server error', err)
+// });
+
+// await new Promise((resolve) => 
+//   app.listen(+process.env.port, process.env.host, () => resolve()));
+
+// export { app }
